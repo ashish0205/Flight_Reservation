43dea489be2ff91ee4ab1f5e99ce7dabdf1ac235
Asmita Gulia <asmita.gulia@gmail.com>
2021-03-25 16:27:34 -0400

    signUp page

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -5,10 +5,10 @@ import { Text, StyleSheet, View,TextInput, Button } from "react-native";
 
 const SignUP = () => {
     const [fname, setFirstName] = useState("");
-   // const [lname, setLastName] = useState("");
-    //const [email, setEmail] = useState("");
-    //const [password, setPassword] = useState("");
-    //const [confirmPassword, setConfirmPassword] = useState("");
+    const [lname, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     return (
         <View style={styles.container}>
@@ -17,18 +17,59 @@ const SignUP = () => {
             
             <TextInput
                 style={styles.TextInput}
-                placeholder="First Name"
+                    placeholder="Enter First Name"
+                    placeholderTextColor="#003f5c"
                 onChangeText={fname => setFirstName(text)}
                 />
-                </View>
+            </View>
+            <View style={styles.inputView}>
+            
+            <TextInput
+                style={styles.TextInput}
+                placeholderTextColor="#003f5c"
+                placeholder="Enter Last Name"
+                onChangeText={lname => setLastName(text)}
+                />
+            </View>
+            <View style={styles.inputView}>
+                
+                <TextInput
+                style={styles.TextInput}    
+                placeholder="Enter Your Email"
+                placeholderTextColor="#003f5c"
+                onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+            <View style={styles.inputView}>
+                <TextInput
+                style={styles.TextInput}
+                placeholder="Enter Password"
+                placeholderTextColor="#003f5c"
+                secureTextEntry={true}
+                onChangeText={(password) => setConfirmPassword(password)}
+                />
+            </View>
+            <View style={styles.inputView}>
+                <TextInput
+                style={styles.TextInput}
+                placeholder="Confirm Password"
+                placeholderTextColor="#003f5c"
+                secureTextEntry={true}
+                onChangeText={(confirmPassword) => setPassword(confirmPassword)}
+                />
+            </View>
+            <Button
+              color="#3c415c"
+              title="SignUp"
+            />
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     heading: {
-      
-        fontSize:25  
+        fontSize: 25,
+        marginBottom:15
     },
     container: {
         flex: 1,
