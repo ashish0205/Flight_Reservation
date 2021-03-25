95be81a5af63bb7d176ed0b0156f4273bfaf3851
Asmita Gulia <asmita.gulia@gmail.com>
2021-03-25 15:51:54 -0400

    test commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ const HomeScreen = ({ navigation }) => {
     <View style={styles.container}>
       <ImageDetails imageSource={require("../../assets/home.jpeg")} />
       <View style={styles.space} />
-      <Text style={styles.text}>Love at First Flight</Text>
+      <Text style={styles.text}>Love at First sight</Text>
       <View style={styles.space} />
       <Button
         style={styles.button}
