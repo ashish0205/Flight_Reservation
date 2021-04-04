24a66dd3e01c8214cebcd4fbdc4873b50e1963fe
Ashish Sharma <ashish.as.sharma999@gmail.com>
2021-04-03 23:38:24 -0400

    ashish commit

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -1,149 +1,167 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, TextInput, Button, _Image,Dimensions } from "react-native";
+import axios from "axios";
+const QueryString = require("querystring");
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  _Image,
+  Dimensions,
+} from "react-native";
 import { TouchableOpacity } from "react-native";
-import * as Animatable from 'react-native-animatable';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import * as Animatable from "react-native-animatable";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const LogIn = ({navigation}) => {
+const LogIn = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Welcome</Text>
+      </View>
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    return (
-        <View style={styles.container}>
+      <Animatable.View style={styles.footer} animation="bounceInLeft">
+        <View style={styles.inputView}>
+          <View style={styles.form}>
+            <StatusBar style="auto" />
 
-            <View style={styles.header}>
-                <Text style={styles.text_header}>Welcome</Text>
+            <View style={styles.action}>
+              <FontAwesome name="user-o" color="#05375a" size={20} />
+              <TextInput
+                style={styles.TextInput}
+                placeholder="Your Email"
+                placeholderTextColor="#003f5c"
+                value={email}
+                autoCorrect={false}
+                onChangeText={(email) => setEmail(email)}
+              />
             </View>
-            
-            <Animatable.View style={styles.footer}
-                animation="bounceInLeft">
-                 <View style={styles.inputView}>
-            <View style={styles.form}>
-              <StatusBar style="auto" />
-                    
-                    <View style={styles.action}>
-                <FontAwesome
-                            name="user-o"
-                            color="#05375a"
-                            size={20}
-                        />
-                <TextInput
-                    style={styles.TextInput}    
-                    placeholder="Your Email"
-                    placeholderTextColor="#003f5c"
-                    value={email}
-                    autoCorrect={false}
-                    onChangeText={(email) => setEmail(email)}
-                />
-                </View>
-          
-                    <View style={styles.action}>
-                    <FontAwesome
-                            name="lock"
-                            color="#05375a"
-                            size={20}
-                        />
-                <TextInput
+
+            <View style={styles.action}>
+              <FontAwesome name="lock" color="#05375a" size={20} />
+              <TextInput
                 style={styles.TextInput}
                 placeholder="Your Password"
                 placeholderTextColor="#003f5c"
                 secureTextEntry={true}
-                    value={password}
-                    autoCorrect={false}    
+                value={password}
+                autoCorrect={false}
                 onChangeText={(password) => setPassword(password)}
-                />
-                </View>
-                <TouchableOpacity onPress={() => navigation.navigate('ForgetPassword')}>
-                <Text style={styles.forgot_button}>Forgot Password?</Text>
-               
+              />
+            </View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("ForgetPassword")}
+            >
+              <Text style={styles.forgot_button}>Forgot Password?</Text>
             </TouchableOpacity>
-            
+
             <Button
               color="#3c415c"
-              title="LogIn"                       
-             />
-             </View>
-            </View>
-        </Animatable.View>          
-
-     </View>
-        
-
-    )
+              title="LogIn"
+              onPress={() => {
+                axios
+                  .post(
+                    "http://localhost:4000/user/login",
+                    QueryString.stringify({
+                      email: email,
+                      password: password,
+                    }),
+                    {
+                      headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                      },
+                    }
+                  )
+                  .then((response) => {
+                    console.log(response.data);
+                    navigation.navigate("Home");
+                  })
+                  .catch((err) => console.log("api Erorr: ", err.response));
+              }}
+            />
+          </View>
+        </View>
+      </Animatable.View>
+    </View>
+  );
 };
 
 const { height } = Dimensions.get("screen");
 const height_logo = height * 0.7 * 0.6;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#05375a",
-      },
-      header: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        paddingHorizontal: 20,
-        paddingBottom: 50,
-        marginTop:20
-      },
-      footer: {
-        flex: 3,
-        backgroundColor: 'white',
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingHorizontal: 20,
-        paddingVertical: 30,
-      },
-    image: {
-        marginTop:10,
-        marginBottom: 20,
-        width: 250,
-        height:150
-    },
-    TextInput: {
-        height:50,
-        flex: 1,
-        paddingLeft: 10,
-        color:'#05375a',
-        marginLeft: 20,
-    
-    },
-    forgot_button: {
-        height: 30,
-        marginBottom: 30,
-    },
-    
-    logo: {
-        width: height_logo,
-        height: height_logo,
-        borderRadius: 40,
-        overflow:"hidden"
-    },
-    form: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text_header: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize:30
-    },
-    text_footer: {
-        color: '#05375a',
-        fontSize:18
-    },
-    action: {
-        flexDirection: 'row',
-        backgroundColor: "#bbbbbb",
-        borderRadius: 30,
-        width: "70%",
-        height: 45,
-        marginBottom: 20,     
-        alignItems: "center",
-        padding:20
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#05375a",
+  },
+  header: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+    marginTop: 20,
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: "white",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  image: {
+    marginTop: 10,
+    marginBottom: 20,
+    width: 250,
+    height: 150,
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    paddingLeft: 10,
+    color: "#05375a",
+    marginLeft: 20,
+  },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  logo: {
+    width: height_logo,
+    height: height_logo,
+    borderRadius: 40,
+    overflow: "hidden",
+  },
+  form: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text_header: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  text_footer: {
+    color: "#05375a",
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: "row",
+    backgroundColor: "#bbbbbb",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+    padding: 20,
+  },
 });
 
 export default LogIn;
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,158 +1,158 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, View,TextInput, Button } from "react-native";
+import { Text, StyleSheet, View, TextInput, Button } from "react-native";
 import axios from "axios";
-import * as Animatable from 'react-native-animatable';
-const QueryString=require("querystring")
+import * as Animatable from "react-native-animatable";
+const QueryString = require("querystring");
 
-const SignUP = () => {
-    const [fname, setFirstName] = useState("");
-    const [lname, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-            <Text style={styles.heading}>To create an account, Insert your Details:</Text>
-            </View>
-            <Animatable.View style={styles.footer} animation="fadeInDownBig">
-              <View style={styles.formData}>
-            <View style={styles.inputView}>
-            
+const SignUP = ({ navigation }) => {
+  const [fname, setFirstName] = useState("");
+  const [lname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.heading}>
+          To create an account, Insert your Details:
+        </Text>
+      </View>
+      <Animatable.View style={styles.footer} animation="fadeInDownBig">
+        <View style={styles.formData}>
+          <View style={styles.inputView}>
             <TextInput
-                style={styles.TextInput}
-                    placeholder="Your First Name"
-                    placeholderTextColor="black"
-                    value={fname}
-                    autoCorrect={false}
-                    onChangeText={(fname) => setFirstName(fname)}
-                />
-            </View>
-            <View style={styles.inputView}>
-            
-            <TextInput
-                style={styles.TextInput}
-                placeholderTextColor="black"
-                    placeholder="Your Last Name"
-                    value={lname}
-                    autoCorrect={false}
-                onChangeText={(lname) => setLastName(lname)}
-                />
-            </View>
-            <View style={styles.inputView}>
-                
-                <TextInput
-                style={styles.TextInput}    
-                placeholder="Enter Your Email"
-                    placeholderTextColor="black"
-                    value={email}
-                    autoCorrect={false}
-                onChangeText={(email) => setEmail(email)}
-                />
-            </View>
-            <View style={styles.inputView}>
-                <TextInput
-                style={styles.TextInput}
-                placeholder="Enter Password"
-                placeholderTextColor="black"
-                    secureTextEntry={true}
-                  
-                    autoCorrect={false}
-                onChangeText={(password) => setConfirmPassword(password)}
-                />
-                {password.length>8?<Text>Password must be 8 character long</Text>:null}
-            </View>
-            <View style={styles.inputView}>
-                <TextInput
-                style={styles.TextInput}
-                placeholder="Confirm Password"
-                placeholderTextColor="black"
-                    secureTextEntry={true}
-                    
-                    autoCorrect={false}
-                onChangeText={(confirmPassword) => setPassword(confirmPassword)}
-                />
-                 {confirmPassword.length>8?<Text>Password must be 8 character long</Text>:null}
-            </View>
-            <Button
-              color="#05375a"
-                title="SignUp"
-                onPress={() => {
-                    //  alert("hello");
-           
-                    axios
-                      .post(
-                        "http://localhost:4000/user/signup",
-                        QueryString.stringify({
-                          firstName: fname,
-                          lastName: lname,
-                          email: email,
-                          password: password,
-                        }),
-                        {
-                          headers: {
-                            "Content-Type": "application/x-www-form-urlencoded",
-                          },
-                        }
-                      )
-                      .then((response) => {
-                          console.log(response.data);
-                          
-                      })
-                      .catch((err) => console.log("api Erorr: ", err.response));
-                  }}
+              style={styles.TextInput}
+              placeholder="Your First Name"
+              placeholderTextColor="black"
+              value={fname}
+              autoCorrect={false}
+              onChangeText={(fname) => setFirstName(fname)}
             />
-            </View>
-        </Animatable.View>
-     </View>    
-    )
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholderTextColor="black"
+              placeholder="Your Last Name"
+              value={lname}
+              autoCorrect={false}
+              onChangeText={(lname) => setLastName(lname)}
+            />
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Enter Your Email"
+              placeholderTextColor="black"
+              value={email}
+              autoCorrect={false}
+              onChangeText={(email) => setEmail(email)}
+            />
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Enter Password"
+              placeholderTextColor="black"
+              secureTextEntry={true}
+              autoCorrect={false}
+              onChangeText={(password) => setConfirmPassword(password)}
+            />
+            {password.length > 8 ? (
+              <Text>Password must be 8 character long</Text>
+            ) : null}
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Confirm Password"
+              placeholderTextColor="black"
+              secureTextEntry={true}
+              autoCorrect={false}
+              onChangeText={(confirmPassword) => setPassword(confirmPassword)}
+            />
+            {confirmPassword.length > 8 ? (
+              <Text>Password must be 8 character long</Text>
+            ) : null}
+          </View>
+          <Button
+            color="#05375a"
+            title="SignUp"
+            onPress={() => {
+              //  alert("hello");
+
+              axios
+                .post(
+                  "http://localhost:4000/user/signup",
+                  QueryString.stringify({
+                    firstName: fname,
+                    lastName: lname,
+                    email: email,
+                    password: password,
+                  }),
+                  {
+                    headers: {
+                      "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                  }
+                )
+                .then((response) => {
+                  console.log(response.data);
+                  navigation.navigate("LogIn");
+                })
+                .catch((err) => console.log("api Erorr: ", err.response));
+            }}
+          />
+        </View>
+      </Animatable.View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    heading: {
-        fontSize: 25,
-        marginBottom: 15,
-        color: 'white',
-        fontWeight:'bold'
-    },
-    container: {
-        flex: 1,
-        backgroundColor: "#1d4b6a",
-      },
-      header: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        paddingHorizontal: 20,
-        paddingBottom: 50,
-      },
-      footer: {
-        flex: 3,
-        backgroundColor: '#b4c3cd',
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingHorizontal: 20,
-        paddingVertical:30
-      },
-    TextInput: {
-        height: 200,
-        width: "100%",
-        flex: 1,
-        padding: 10,
-        marginLeft: 20,
-    },
-    inputView: {
-        backgroundColor: "#e6ebee",
-        borderRadius: 30,
-        width: "50%",
-        height: 45,
-        marginBottom: 20,     
-       
-    },
-    formData: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  heading: {
+    fontSize: 25,
+    marginBottom: 15,
+    color: "white",
+    fontWeight: "bold",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#1d4b6a",
+  },
+  header: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: "#b4c3cd",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  TextInput: {
+    height: 200,
+    width: "100%",
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  inputView: {
+    backgroundColor: "#e6ebee",
+    borderRadius: 30,
+    width: "50%",
+    height: 45,
+    marginBottom: 20,
+  },
+  formData: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default SignUP;
